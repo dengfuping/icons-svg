c56c1df03de2342ff349cf58b252149f50926f63
dengfuping <dengfuping_private@163.com>
2023-06-15 21:15:47 +0800

    improve: Use `0 0 1024 1024` viewBox for special icons

--- src/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
+++ src/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
@@ -1,6 +1,25 @@
 import { assignAttrsAtTag } from "..";
 import { TransformFactory } from "../..";
 
-export const adjustViewBox: TransformFactory = assignAttrsAtTag("svg", () => ({
-  viewBox: "64 64 896 896",
-}));
+// icons with `0 0 1024 1024` viewBox
+const SPECIAL_ICON_NAMES = [
+  "settings",
+  "douyin",
+  "numeral",
+  "letter",
+  "letter-lower",
+  "letter-upper",
+  "funtion-bold",
+  "sql",
+  "timer",
+  "wifi",
+];
+
+export const adjustViewBox: TransformFactory = assignAttrsAtTag(
+  "svg",
+  ({ name }) => ({
+    viewBox: SPECIAL_ICON_NAMES.includes(name)
+      ? "0 0 1024 1024"
+      : "64 64 896 896",
+  })
+);
